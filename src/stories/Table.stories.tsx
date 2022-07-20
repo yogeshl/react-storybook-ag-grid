dfa02e5bdae1755bf94e10ca2beda7d9256ed382
Yogesh Linganna <yogesh.linganna@gmail.com>
2022-07-20 16:12:07 -0400

    Adding Custom Cell Renders and Cell Styles

--- src/stories/Table.stories.tsx
+++ src/stories/Table.stories.tsx
@@ -3,6 +3,10 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Table from "./Table";
 import { TableType } from "./Table.types";
 
+import Car from "./assets/sedan.png";
+import Jeep from "./assets/jeep.png";
+import Van from "./assets/van.png";
+
 // define defaults that can be used as a base for each story
 const defaultArgs: TableType = {
 	isDark: false,
@@ -78,4 +82,37 @@ function getData() {
         	});
 	}
 	return data;
+};
+
+
+
+// this function will determine the cell’s styling, based on the value
+const carPrice = (value: number) => {
+    return { color: value > 50000 ? "#FF5733" : "#00E676" };
+};
+
+// this function will tell the cell to render as an image corresponding to the value of the cell  
+const carType = (value: string) => {
+    return <img 
+        alt="" 
+        src={value === "sedan" ? Car : value === "jeep" ? Jeep : Van} 
+        style={{ width: "24px", height: "24px" }} 
+    />;
+};
+
+export const CustomCellRender = Template.bind({});
+CustomCellRender.args = {
+    ...defaultArgs,
+    isDark: false,
+    rowData: [
+        { make: "Toyota", model: "Celica", price: 35000, type: "sedan" },
+        { make: "Nissan", model: "KDH", price: 32000, type: "van" },
+        { make: "KIA", model: "Sorento", price: 72000, type: "jeep" }
+    ],
+    columnDefs: [
+        { field: "make" },
+        { field: "model" },
+        { field: "price", cellStyle: (params: any) => carPrice(params.value) },
+        { field: "type", cellRenderer: (params: any) => carType(params.value) },
+    ]
 };
